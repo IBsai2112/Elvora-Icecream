0bc8fc727d9058e605d95d187367463e9604dcb5
Sainjal Kalnekar <sainjalkalnekar21@gmail.com>
2026-03-14 22:17:57 +0530

    Creating a basic footer" - just for inilize

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -3,6 +3,7 @@ import CoverVideo from '../components/coverVideo';
 import Navbar from '../components/navbar';
 import Trending from '../sections/Trending';
 import Trending2 from '../sections/Trending2';
+import Footer from '../sections/Footer';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <CoverVideo />
         <Trending />
         <Trending2 />
+        <Footer />
     </div>
   )
 }
